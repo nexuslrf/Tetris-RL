c6aa5fe54ef3edbf37bc390ae2b48a5043f542e9
nexuslrf <shootingstarlrf@hotmail.com>
2020-12-08 01:29:43 +0800

    Proirity DQN

--- train_rl.py
+++ train_rl.py
@@ -2,6 +2,7 @@ from timeit import default_timer as timer
 from datetime import timedelta
 import numpy as np
 import random
+import os
 
 from MaTris.gym_matris import MatrisEnv
 from agents.DQN import Model
@@ -10,7 +11,9 @@ from utils.ReplayMemory import ExperienceReplayMemory
 
 if __name__ == "__main__":
     config = Config()
-    env = MatrisEnv(no_display=False)
+    config.USE_NOISY_NETS = True
+    config.USE_PRIORITY_REPLAY = True
+    env = MatrisEnv(no_display=True)
     model = Model(env=env, config=config)
 
     episode_reward = 0
@@ -47,7 +50,8 @@ if __name__ == "__main__":
 
         # if frame_idx % 10000 == 0:
         #     plot(frame_idx, model.rewards, model.losses, model.sigma_parameter_mag, timedelta(seconds=int(timer()-start)))
-
+    if not os.path.exists('saved_agents/'):
+        os.makedirs('saved_agents')
     model.save_w()
     env.close()
 
